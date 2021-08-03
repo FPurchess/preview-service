2f2b9eb60c76945ce5a1e6a4f7b88b191a7bdfb1
Florian Purchess <florian@purchess.de>
2021-08-03 19:11:42 +0200

    format: black

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ from starlette.responses import (
 from preview_generator.manager import PreviewManager
 
 
-UPLOAD_DIR = '/tmp/files/'
-CACHE_PATH = '/tmp/cache/'
+UPLOAD_DIR = "/tmp/files/"
+CACHE_PATH = "/tmp/cache/"
 
 
 app = Starlette()
-app.mount('/cache', StaticFiles(directory=CACHE_PATH), name='cache')
+app.mount("/cache", StaticFiles(directory=CACHE_PATH), name="cache")
 
 
 manager = PreviewManager(CACHE_PATH, create_folder=True)
@@ -35,26 +35,26 @@ def error_response(error_message, status=None):
 async def _store_uploaded_file(file) -> str:
     contents = await file.read()
     h = hashlib.md5(contents).hexdigest()
-    upload_dest = os.path.join(UPLOAD_DIR, '.'.join([h, file.filename]))
+    upload_dest = os.path.join(UPLOAD_DIR, ".".join([h, file.filename]))
 
-    with open(upload_dest, 'wb+') as f:
+    with open(upload_dest, "wb+") as f:
         f.write(contents)
 
     return upload_dest
 
 
-@app.route('/')
+@app.route("/")
 async def health_endpoint(request):
-    return PlainTextResponse('OK')
+    return PlainTextResponse("OK")
 
 
-@app.route('/preview/{width:int}x{height:int}', methods=['POST'])
+@app.route("/preview/{width:int}x{height:int}", methods=["POST"])
 async def preview_endpoint(request):
-    width = request.path_params['width']
-    height = request.path_params['height']
+    width = request.path_params["width"]
+    height = request.path_params["height"]
 
     form = await request.form()
-    file = form.get('file', None)
+    file = form.get("file", None)
     if file is None:
         return error_response('"file" is missing', status.HTTP_400_BAD_REQUEST)
     file_path = await _store_uploaded_file(file)
@@ -68,4 +68,4 @@ async def preview_endpoint(request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
